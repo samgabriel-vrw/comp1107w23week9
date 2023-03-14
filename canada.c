70695539adfd4f6e4c2bca0f40a42c8b606c1989
samgabriel-vrw <81274739+samgabriel-vrw@users.noreply.github.com>
2023-03-14 16:42:02 -0400

    Add files via upload

---
+++ canada.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+const char *PROVINCES[] = {"Alberta", "British Columbia", "Manitoba", "New Brunswick", "Newfoundland and Labrador", "Nova Scotia", "Ontario", "Prince Edward Island", "Quebec", "Saskatchewan"};
+
+void print_provinces()
+{
+    // TODO:: Write a for loop that iterates over the PROVINCES array and prints out
+    //        how many letters is contained in each.
+
+    int province_size = sizeof(PROVINCES) / sizeof(PROVINCES[0]);
+
+    for (int i = 0; i < province_size; i++)
+    {
+    }
+}
